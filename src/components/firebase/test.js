72b04f79093594279e6a719ed46d2779e54ba1e1
febasusansunny <febasusansunny@users.noreply.github.com>
2021-03-07 15:08:04 +0530

    Delete src directory

--- src/components/firebase/test.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
-
-const firestore = firebase.firestore();
-
-firestore.collection('users').doc('tZoSQ1YJuoV4yjXU7u8y').collection('cartItems').doc('ppqhm2rIFrfDYJw4h5pA')
-
-firestore.doc('/users/GSVqgBMaqk4ZlHOIcqrC/cartItems/96HRMUwkzdYIPiAKMbok/')
